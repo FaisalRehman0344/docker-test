9162ebe272e3502086a90d8d8512ca3e6c723d98
unknown <product@minibigtech.com>
2022-01-30 17:56:21 +0500

    updated controllers

--- src/main/java/com/example/dockertest/DockerTestApplication.java
+++ src/main/java/com/example/dockertest/DockerTestApplication.java
@@ -3,6 +3,7 @@ package com.example.dockertest;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -14,6 +15,11 @@ public class DockerTestApplication {
 	@GetMapping
 	public String test(){
 		return "Working Docker";
+	}
+
+	@GetMapping(value = "/{m}")
+	public String test(@PathVariable String m){
+		return "Working "+m;
 	}
 
 	public static void main(String[] args) {
